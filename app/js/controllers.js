b1870b4bccc880c4a9589489ff258f0412d54386
mattiaS <mattia@3doers.it>
2016-12-26 13:59:07 +0100

    chore(cleanup): more cleanup

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,10 +1,9 @@
- /* eslint-disable */
-import Task from 'data.task'
-import _ from 'ramda'
+import Task from 'data.task';
+import _ from 'ramda';
 
 import * as db from './db';
 import * as H from './helpers';
-import { updateDom, clearAll } from './updateViewHelpers';
+import { updateDom } from './updateViewHelpers';
 import * as rH from './renderHelpers';
 import animate from './animation.js';
 
@@ -14,31 +13,33 @@ export const list = current => H.compose(
   updateDom,
   rH.renderHistory,
   H.genArrayOfLiComponents(rH.renderInvite),
-  //H.sortByDescendingTime,
-  H.sortByReverseInviteId, 
+  // H.sortByDescendingTime,
+  H.sortByReverseInviteId,
   H.parse,
 )(current);
 
 export const mergeData = _.curry((history, update) => (
-  H.compose( 
-    //   H.sortByDescendingTime,
+  H.compose(
+    // H.sortByDescendingTime,
     H.sortByReverseInviteId,
     H.trace('tracing merge  result: '),
     H.mergeRepeatedObjects,
     H.sortByInviteId,
     H.concat(JSON.parse(history)),
     H.markUpdates)(JSON.parse(update))
-))
+));
 
 export const run = () => {
   db.getData('history')
-    .fork(err => console.log(err), list )
-}
+    .fork(err => ((err) ? console.log(err) : list));
+};
 
 export const fetchUpdate = () => {
   Task.of(mergeData)
     .ap(db.getData('history'))
     .ap(db.getData('update'))
-    .fork(err => console.log(err),res =>{ list(res)} )
-}
+  /*eslint-disable */
+    .fork(err => console.log(err), res => { list(res) });
+    /*eslint-enable */
+};
 
