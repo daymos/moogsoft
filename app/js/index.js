5f2e70917189d89d9b5a4c2955427ef83cfbb415
mattiaS <mattia@3doers.it>
2016-12-26 13:48:08 +0100

    chore(cleanup): cleanup)

--- app/js/index.js
+++ app/js/index.js
@@ -1,13 +1,10 @@
- /*eslint-disable*/
 import '../css/style.css';
 import '../vendor/inViewport';
 
 import * as ctrls from './controllers';
-import * as dom from './updateViewHelpers';
-import * as db from './db';
 
 window.onload = () => {
-  ctrls.run()
-  setTimeout( ctrls.fetchUpdate, 2000)
+  ctrls.run();
+  setTimeout(ctrls.fetchUpdate, 2000);
 };
 
